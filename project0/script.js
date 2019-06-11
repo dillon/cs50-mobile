f78dad76c1f2f75104bdbf445d232c1556d4dbc8
dillon <dpett122@gmail.com>
2019-06-11 03:28:35 -0400

    fix

--- project0/script.js
+++ project0/script.js
@@ -1,84 +1,69 @@
-const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
-}
+let itemCount = 0;
+let uncheckedCount = 0;
+const list = document.getElementById('todo-list');
+const itemCountSpan = document.getElementById('item-count');
+const uncheckedCountSpan = document.getElementById('unchecked-count');
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
-let uncheckedCount = 0
-let itemCount = 0
-let key = 0
-
-function newTodo() {
-  const todo = prompt('Add a Todo')
-  if (todo) {
-    return addTodo(todo)
-  }
-  else { return }
+function renderCounters() {
+  uncheckedCountSpan.innerHTML = uncheckedCount;
+  itemCountSpan.innerHTML = itemCount;
 }
 
 function addTodo(todo) {
+  itemCount += 1;
+  uncheckedCount += 1;
 
-  key++
-  uncheckedCount++
-  itemCount++
+  // Create list item
+  const li = document.createElement('li');
 
-  const li = document.createElement('li')
-  li.setAttribute('class', classNames.TODO_ITEM)
 
-  const div = document.createElement('div')
-  div.setAttribute('class', classNames.TODO_TEXT)
-  div.textContent = todo
-  li.appendChild(div)
-
-  const input = document.createElement('input')
-  input.setAttribute('type', 'checkbox')
-  input.setAttribute('class', classNames.TODO_CHECKBOX)
-  input.addEventListener('click', function(event) {
+  // Create checkbox inside list item
+  const input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
+  input.addEventListener('click', (event) => {
     if (event.target.checked) {
-      uncheckedCount--
-      renderCounters()
+      uncheckedCount -= 1;
+      event.target.parentElement.childNodes[1].setAttribute('class', 'isChecked');
     }
     else {
-      uncheckedCount++
-      renderCounters()
+      uncheckedCount += 1;
+      event.target.parentElement.childNodes[1].setAttribute('class', '');
     }
-  })
-  li.appendChild(input)
+    renderCounters();
+  });
+  li.appendChild(input);
 
-  const button = document.createElement('button')
-  button.setAttribute('class', classNames.TODO_DELETE)
-  button.setAttribute('title', 'delete')
-  button.textContent = 'delete'
-  button.addEventListener('click', function(event) {
+  // Create text div inside list item
+  const div = document.createElement('div');
+  div.textContent = todo;
+  li.appendChild(div);
+
+  // Create delete button inside list item
+  const button = document.createElement('button');
+  button.setAttribute('title', 'delete');
+  button.textContent = 'delete';
+  button.addEventListener('click', () => {
+    // delete button callback
     const confirmation = confirm('Are you sure you want to delete this Todo?')
     if (confirmation) {
-      li.parentNode.removeChild(li)
-      uncheckedCount--
-      itemCount--
-      renderCounters()
+      if (!li.childNodes[0].checked) {
+        // if checkbox was unchecked, decrement unchecked by one
+        uncheckedCount -= 1;
+      }
+      li.parentNode.removeChild(li);
+      itemCount -= 1;
+      renderCounters();
     }
-  })
-  li.appendChild(button)
+  });
+  li.appendChild(button);
 
-  list.appendChild(li)
-  renderCounters()
+  // Append list item to list
+  list.appendChild(li);
+  renderCounters();
 }
 
-function deleteConfirm(todo) {
-  var confirmation = confirm('Delete this Todo?')
-  if (confirmation) {
-    uncheckedCount--
-    itemCount--
-    renderCounters()
-    todo.parentNode.removeChild(todo)
-  }
-}
-
-function renderCounters() {
-  uncheckedCountSpan.innerHTML = uncheckedCount
-  itemCountSpan.innerHTML = itemCount
+function newTodo() {
+  // function called from HTML
+  const todo = prompt('Add a Todo');
+  if (todo) addTodo(todo);
 }
